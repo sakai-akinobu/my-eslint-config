131354b24fc83b86f52b2d344d2f63fcf851bdc1
sakai-akinobu <sakai.akinobu@gmail.com>
2018-11-10 18:46:37 +0900

    Add `no-console` rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "no-async-promise-executor": "error",
     "no-compare-neg-zero": "error",
     "no-cond-assign": "error",
+    "no-console": "error",
     // Stylistic Issues
     "semi": "error",
   },
