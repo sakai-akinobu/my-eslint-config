ced679226d5aedf2a002859a8fee187e5d8f8fa5
sakai-akinobu <sakai.akinobu@gmail.com>
2018-11-10 18:22:07 +0900

    Fix test case scope.

--- tests/index.js
+++ tests/index.js
@@ -3,15 +3,15 @@ const path = require('path');
 const klawSync = require('klaw-sync');
 const assert = require('power-assert');
 
-describe('index.js', function() {
-  it('all', function() {
-    const fixtureDir = path.join(__dirname, '..', 'fixtures');
-    const fixtures = klawSync(fixtureDir, {nodir: true}).
-      map(file => file.path).
-      filter(path => path.endsWith('.js'));
+const fixtureDir = path.join(__dirname, '..', 'fixtures');
+const fixtures = klawSync(fixtureDir, {nodir: true}).
+  map(file => file.path).
+  filter(path => path.endsWith('.js'));
 
-    fixtures.forEach(fixture => {
-      const ruleName = path.basename(path.resolve(fixture, '..'));
+describe('index.js', function() {
+  fixtures.forEach(fixture => {
+    const ruleName = path.basename(path.resolve(fixture, '..'));
+    it(ruleName, function() {
       assert.throws(() => {
         execSync(`npx eslint --format json ${fixture}`).toString();
       }, (e) => {
