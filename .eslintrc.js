cc6b197989f062a70a6a0c782fba688385f9addf
sakai-akinobu <sakai.akinobu@gmail.com>
2018-11-10 18:18:35 +0900

    Add `no-async-promise-executor` rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
   rules: {
     // Possible Errors
     "for-direction": "error",
     "getter-return": "error",
+    "no-async-promise-executor": "error",
     // Stylistic Issues
     "semi": "error",
   },
