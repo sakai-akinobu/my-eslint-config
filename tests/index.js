a0d9dc8bc62ad57ec8af7b06302a6455cb35c217
sakai-akinobu <sakai.akinobu@gmail.com>
2018-11-17 11:05:25 +0900

    Rename fixtures.

--- tests/index.js
+++ tests/index.js
@@ -3,21 +3,25 @@ const path = require('path');
 const klawSync = require('klaw-sync');
 const assert = require('power-assert');
 
-const fixtureDir = path.join(__dirname, '..', 'fixtures');
-const fixtures = klawSync(fixtureDir, {nodir: true}).
-  map(file => file.path).
-  filter(path => path.endsWith('.js'));
+function getFixtures(lang) {
+  const fixtureDir = path.join(__dirname, '..', 'fixtures', lang);
+  return klawSync(fixtureDir, {nodir: true}).
+    map(file => file.path).
+    filter(path => path.endsWith('.js'));
+}
 
 describe('index.js', function() {
-  fixtures.forEach(fixture => {
-    const ruleName = path.basename(path.resolve(fixture, '..'));
-    it(ruleName, function() {
-      assert.throws(() => {
-        execSync(`npx eslint --format json ${fixture}`).toString();
-      }, (e) => {
-        eslintError = JSON.parse(e.output[1].toString())[0];
-        return eslintError.messages.some(m => m.ruleId === ruleName);
-      }, `A voilotion of the ${ruleName} rule has not been detected.`);
+  describe('javascript', function() {
+    getFixtures('javascript').forEach(fixture => {
+      const ruleName = path.basename(path.resolve(fixture, '..'));
+      it(ruleName, function() {
+        assert.throws(() => {
+          execSync(`npx eslint --config ./eslintrcs/.eslintrc.base.js  --format json ${fixture}`).toString();
+        }, (e) => {
+          eslintError = JSON.parse(e.output[1].toString())[0];
+          return eslintError.messages.some(m => m.ruleId === ruleName);
+        }, `A voilotion of the ${ruleName} rule has not been detected.`);
+      });
     });
   });
 });
